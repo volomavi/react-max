10483153e1c0981e583b9e6b2bed7161704fef75
Ivan Bogomolov <volomavi@hotmail.com>
2022-10-05 22:07:12 -0700

    added more components and dummy data

--- react-meet/src/App.js
+++ react-meet/src/App.js
@@ -1,13 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
-
 import NewMeetupPage from "./pages/NewMeetup";
 import FavouritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
---
+++ react-meet/src/components/meetups/MeetupList.js
@@ -0,0 +1,21 @@
+import MeetupItems from "./MeetupItems";
+import classes from "./MeetupList.module.css";
+
+function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItems
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+          description={meetup.description}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default MeetupList;
