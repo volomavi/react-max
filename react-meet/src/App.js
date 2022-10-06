785f3150f8c57a36b4f19a62e87c5f2982fca67b
Ivan Bogomolov <volomavi@hotmail.com>
2022-10-05 18:01:41 -0700

    setup router base

---
+++ react-meet/src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
